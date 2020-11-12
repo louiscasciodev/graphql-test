56bbd44fbacaebbfea3f12b69758046b9147a6b0
dolfant <louis.cascio@hotmail.fr>
2020-11-12 11:16:38 +0100

    graphql init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
+
+import { ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://api.spacex.land/graphql/',
+  cache: new InMemoryCache()
+});
+
+// const client = ...
+client
+  .query({
+    query: gql`
+      query GetRates {
+        launches(limit: 5) {
+          launch_date_utc
+          launch_success
+          rocket {
+            rocket_name
+          }
+          links {
+            video_link
+          }
+          details
+        }
+      }`
+  })
+  .then(result => console.log(result));
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    launches(limit: 5) {
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.launches.map(({ launch_date_utc, launch_success, rocket, links, details }) => (
+    <div key={rocket.rocket_name}>
+      <p>
+        {rocket.rocket_name}
+        {links.video_link}
+        {launch_date_utc}
+        {launch_success}
+        {details}
+      </p>
+    </div>
+  ));
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <ExchangeRates />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
